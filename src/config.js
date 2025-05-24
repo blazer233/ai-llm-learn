ce8c5c020406ed4b9d956732c531000705f1f5cf
darylsong <darylsong@tencent.com>
2025-05-24 13:09:37 +0800

    feat:更新内容

--- src/config.js
+++ src/config.js
@@ -1,30 +1,77 @@
 import { TencentHunyuanEmbeddings } from '@langchain/community/embeddings/tencent_hunyuan';
+import { AlibabaTongyiEmbeddings } from '@langchain/community/embeddings/alibaba_tongyi';
 import { ChatDeepSeek } from '@langchain/deepseek';
 import { ChatOllama, OllamaEmbeddings } from '@langchain/ollama';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import 'dotenv/config';
 
 export const SPLIT_SIGN = '===SPLIT===';
 
+/**
+ * 语言模型 model
+ */
+// 本地模型
 export const model = new ChatOllama({
   baseUrl: 'http://localhost:11434',
   model: process.env.MODEL_NAME,
   temperature: 0.2,
 });
+
+// DeepSeek 模型
 // export const model = new ChatDeepSeek({
 //   apiKey: process.env.DEEPSEEK_API_KEY, // 从环境变量获取API key
 //   model: 'deepseek-chat', // 指定DeepSeek模型
 //   temperature: 0.2,
 // });
+
+// 华为云模型
+// export const model = new ChatOpenAI({
+//   model: 'DeepSeek-V3',
+//   openAIApiKey: process.env.HUAWEI_KEY,
+//   configuration: {
+//     baseURL: 'https://api.modelarts-maas.com/v1',
+//   },
+// });
+
+// 百度模型
+// export const model = new ChatOpenAI({
+//   model: 'DeepSeek-V3',
+//   openAIApiKey: process.env.BAIDU_KEY,
+//   configuration: {
+//     baseURL: 'https://aistudio.baidu.com/llm/lmapi/v3',
+//   },
+// });
+
+/**
+ * 向量模型 embedding
+ */
+
+// 本地向量模型
 export const embedding = new OllamaEmbeddings({
   model: 'bge-m3',
   baseUrl: 'http://localhost:11434',
 });
+
+// 百度向量模型
+// export const embedding = new OpenAIEmbeddings({
+//   model: 'embedding-v1',
+//   openAIApiKey: process.env.BAIDU_KEY,
+//   configuration: {
+//     baseURL: 'https://aistudio.baidu.com/llm/lmapi/v3',
+//   },
+// });
+
+/**以下已欠费 */
+
+// 腾讯云向量模型
 // export const embedding = new TencentHunyuanEmbeddings({
 //   model: 'hunyuan-embedding',
 //   streaming: false,
 //   tencentSecretId: process.env.HUNYUAN_ID,
 //   tencentSecretKey: process.env.HUNYUAN_KEY,
 // });
+
+// 阿里云向量模型
 // export const embedding = new AlibabaTongyiEmbeddings({
 //   apiKey: process.env.QWEN_API_KEY,
 //   modelName: 'text-embedding-v3',
--- src/test.js
+++ src/test.js
@@ -1,43 +1,20 @@
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
-import { TencentHunyuanEmbeddings } from '@langchain/community/embeddings/tencent_hunyuan';
-import 'dotenv/config';
-import { HumanMessage } from '@langchain/core/messages';
+import { embedding } from './config.js';
 
-const messages = [new HumanMessage('介绍你自己')];
-
-const hunyuanEmbedding = new TencentHunyuanEmbeddings({
-  model: 'hunyuan-embedding',
-  streaming: false,
-  tencentSecretId: process.env.HUNYUAN_ID,
-  tencentSecretKey: process.env.HUNYUAN_KEY,
-});
-
-// hunyuanLite.invoke(messages).then(docs => {
-//   console.log(docs);
-//   process.exit(0);
-// });
-// chat.invoke(messages).then(docs => {
-//   console.log(docs);
-//   process.exit(0);
-// });
+const texts = [
+  'LangChain 是一个用于构建 LLM 应用的框架。',
+  'MemoryVectorStore 是一个基于内存的向量存储。',
+  'OpenAIEmbeddings 用于将文本转换成向量。',
+];
 MemoryVectorStore.fromTexts(
-  [
-    '建筑物由砖块建成',
-    '建筑物由木材建成',
-    '建筑物由石头建成',
-    '汽车由金属制成',
-    '汽车由塑料制成',
-    '线粒体是细胞的动力工厂',
-    '线粒体由脂质构成',
-  ],
-  [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }],
-  hunyuanEmbedding
+  texts,
+  texts.map(i => ({ id: i + 1 })),
+  embedding
 ).then(res => {
   res
-    .asRetriever(5)
-    .invoke('汽车由什么制成')
+    .asRetriever(1)
+    .invoke('OpenAIEmbeddings 是做什么的')
     .then(docs => {
       console.log(docs);
-      process.exit(0);
     });
 });
